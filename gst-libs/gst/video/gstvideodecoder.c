71745a77db2c246aee48526c00813788f5efa710
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2018-09-07 22:15:35 -0400

    videodecoder: Delete the link before pushing
    The gst_video_decoder_clip_and_push_buf() now drops the internal stream
    lock while pushing. This means, the output_queued list could be modififed
    during that time. To make the code safe again, we delete the link before
    pushing the data. The walk pointer will later be updated with the list
    head, which makes it safe in case the list was modififed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=715192
    
    Upstream-Status: Upstream
    Signed-off-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>

--- gst-libs/gst/video/gstvideodecoder.c
+++ gst-libs/gst/video/gstvideodecoder.c
@@ -2337,6 +2337,9 @@ gst_video_decoder_flush_parse (GstVideoDecoder * dec, gboolean at_eos)
       while (walk) {
         GstBuffer *buf = GST_BUFFER_CAST (walk->data);
 
+        priv->output_queued =
+            g_list_delete_link (priv->output_queued, priv->output_queued);
+
         if (G_LIKELY (res == GST_FLOW_OK)) {
           /* avoid stray DISCONT from forward processing,
            * which have no meaning in reverse pushing */
@@ -2361,8 +2364,6 @@ gst_video_decoder_flush_parse (GstVideoDecoder * dec, gboolean at_eos)
           gst_buffer_unref (buf);
         }
 
-        priv->output_queued =
-            g_list_delete_link (priv->output_queued, priv->output_queued);
         walk = priv->output_queued;
       }
 

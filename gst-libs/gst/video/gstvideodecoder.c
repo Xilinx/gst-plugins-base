f56c3089a0f03cbe0c2fa4f8b90e4c4025bcfec7
Guillaume Desmottes <guillaume.desmottes@collabora.com>
2020-03-19 10:25:34 -0700

    HACK: videodecoder: fix event ordering with XLNXLL
    When using XLNXLL the OMX decoder will produce its frame before
    it has received all the slices. As a result the frame won't appear in
    priv->frames. But we still need to push its events to not break the
    ordering.
    
    Upstream-Status: Invalid [HACK]
    Signed-off-by: Guillaume Desmottes <guillaume.desmottes@collabora.com>
    Signed-off-by: Dylan Yip <dylan.yip@xilinx.com>

--- gst-libs/gst/video/gstvideodecoder.c
+++ gst-libs/gst/video/gstvideodecoder.c
@@ -2682,6 +2682,7 @@ gst_video_decoder_prepare_finish_frame (GstVideoDecoder *
   GstVideoDecoderPrivate *priv = decoder->priv;
   GList *l, *events = NULL;
   gboolean sync;
+  gboolean found_frame = FALSE;
 
 #ifndef GST_DISABLE_GST_DEBUG
   GST_LOG_OBJECT (decoder, "n %d in %" G_GSIZE_FORMAT " out %" G_GSIZE_FORMAT,
@@ -2707,8 +2708,20 @@ gst_video_decoder_prepare_finish_frame (GstVideoDecoder *
       tmp->events = NULL;
     }
 
-    if (tmp == frame)
+    if (tmp == frame) {
+      found_frame = TRUE;
       break;
+    }
+  }
+
+  /* When using XLNXLL the OMX decoder will produce its frame before it has
+   * received all the slides. As a result the frame won't appear in
+   * priv->frames. But we still need to push its events to not break the
+   * ordering. */
+  if (!found_frame) {
+    GST_LOG_OBJECT (decoder, "finishing an incomplete frame (XLNXLL)");
+    events = g_list_concat (frame->events, events);
+    frame->events = NULL;
   }
 
   if (dropping || !decoder->priv->output_state) {
